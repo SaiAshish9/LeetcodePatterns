d8bec72c7f3635abe0f9acf0ce0c2ab91d4fd855
saiashish9 <saiashish3760@gmail.com>
2025-09-04 14:59:03 +0530

    feat(leetcode-patterns): updated two pointers solutions

--- twoPointers/q5/ManachersAlgorithm.cpp
+++ twoPointers/q5/ManachersAlgorithm.cpp
@@ -1,6 +1,38 @@
 #include <iostream>
 using namespace std;
 
-int main() {
-    cout << "Hello, World!" << endl;
+string longestPalindrome(string s)
+{
+    string t = preprocess(s);
+    int center = 0, R = 0, C = 0, maxLen = 0, start = 0;
+    int n = t.size();
+    int *P = new int[n];
+    for (int i = 0; i < n; i++)
+        P[i] = 0;
+
+    for (int i = 1; i < n - 1; i++)
+    {
+    }
+
+    delete[] P;
+}
+
+string preprocess(string s)
+{
+    int n = s.size();
+    if (n == 0)
+        return "##";
+    string temp = "#";
+    for (int i = 0; i < n; i++)
+    {
+        temp += "#" + s[i];
+    }
+    temp += "#";
+    return temp;
+}
+
+int main()
+{
+    string s = "babad";
+    cout << longestPalindrome(s) << endl;
 }
