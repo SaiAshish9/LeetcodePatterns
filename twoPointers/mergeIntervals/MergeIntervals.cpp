e7f7fbd87d2ae300eb4e3e1ba7794a4a10cdbbd1
saiashish9 <saiashish3760@gmail.com>
2025-09-04 17:01:23 +0530

    feat(leetcode-patterns): updated two pointers solutions

---
+++ twoPointers/mergeIntervals/MergeIntervals.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main()
+{
+    string s = "babad";
+    cout << longestPalindrome(s) << endl; // "bab"
+}
---
+++ twoPointers/mergeIntervals/MergeIntervals.java
@@ -0,0 +1,33 @@
+package twoPointers.mergeIntervals;
+
+import java.util.*;
+
+public class MergeIntervals {
+
+    private static int[][] merge(int[][] intervals) {
+        int[] current = intervals[0];
+
+        List<int[]> merged = new ArrayList<>();
+
+        for (int i = 1; i < intervals.length; i++) {
+            int[] next = intervals[i];
+
+            if (next[0] <= current[1]) {
+                current[1] = Math.max(current[1], next[1]);
+            } else {
+                merged.add(current);
+                current = next;
+            }
+        }
+
+        merged.add(current);
+        return merged.toArray(new int[merged.size()][]);
+    }
+
+    public static void main(String[] args) {
+        int[][] intervals = { { 1, 3 }, { 2, 6 }, { 8, 10 }, { 15, 18 } };
+        System.out.println(Arrays.deepToString(merge(intervals)));
+        // Output: [[1, 6], [8, 10]]
+    }
+
+}
