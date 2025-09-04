52cdac022e71394a78e493a8d8b61815c5d790f4
saiashish9 <saiashish3760@gmail.com>
2025-09-04 17:39:25 +0530

    feat(leetcode-patterns): updated two pointers solutions

--- twoPointers/mergeIntervals/MergeIntervals.c
+++ twoPointers/mergeIntervals/MergeIntervals.c
@@ -3,12 +3,12 @@
 
 int comparator(const void *a, const void *b)
 {
-    int *x = *(int **)a;
-    int *y = *(int **)b;
+    const int *x = (const int *)a;
+    const int *y = (const int *)b;
     return x[0] - y[0];
 }
 
-int **merge(int **intervals, int n, int *returnSize)
+int **merge(int intervals[][2], int n, int *returnSize)
 {
     if (n == 0)
     {
@@ -16,50 +16,44 @@ int **merge(int **intervals, int n, int *returnSize)
         return NULL;
     }
 
-    qsort(intervals, n, sizeof(int *), comparator);
+    qsort(intervals, n, sizeof(intervals[0]), comparator);
 
     int **merged = (int **)malloc(n * sizeof(int *));
-    int index = 0;
-
-    merged[index] = (int *)malloc(2 * sizeof(int));
-    merged[index][0] = intervals[0][0];
-    merged[index][1] = intervals[0][1];
+    int idx = 0;
+    merged[idx] = (int *)malloc(2 * sizeof(int));
+    merged[idx][0] = intervals[0][0];
+    merged[idx][1] = intervals[0][1];
 
     for (int i = 1; i < n; i++)
     {
         int start = intervals[i][0];
         int end = intervals[i][1];
 
-        if (start <= merged[index][1])
+        if (start <= merged[idx][1])
         {
-            if (end > merged[index][1])
-                merged[index][1] = end;
+            if (end > merged[idx][1])
+                merged[idx][1] = end;
         }
         else
         {
-            index++;
-            merged[index] = (int *)malloc(2 * sizeof(int));
-            merged[index][0] = start;
-            merged[index][1] = end;
+            idx++;
+            merged[idx] = (int *)malloc(2 * sizeof(int));
+            merged[idx][0] = start;
+            merged[idx][1] = end;
         }
     }
 
-    *returnSize = index + 1;
+    *returnSize = idx + 1;
     return merged;
 }
 
 int main()
 {
-    int a1[] = {1, 3};
-    int a2[] = {2, 6};
-    int a3[] = {8, 10};
-    int a4[] = {15, 18};
-
-    int *intervals[4];
-    intervals[0] = a1;
-    intervals[1] = a2;
-    intervals[2] = a3;
-    intervals[3] = a4;
+    int intervals[4][2] = {
+        {1, 3},
+        {2, 6},
+        {8, 10},
+        {15, 18}};
 
     int returnSize;
     int **result = merge(intervals, 4, &returnSize);
