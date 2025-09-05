59bcb38c818687dbfdeb793028211cab55f21bd1
saiashish9 <saiashish3760@gmail.com>
2025-09-06 00:54:57 +0530

    feat(leetcode-patterns): updated two pointers solutions

--- twoPointers/q141/LinkedListCycle.java
+++ twoPointers/q141/LinkedListCycle.java
@@ -2,8 +2,38 @@ package twoPointers.q141;
 
 public class LinkedListCycle {
 
-    public static void main(String[] args) {
+    static class ListNode {
+        int val;
+        ListNode next;
+        ListNode(int x) {
+            val = x;
+            next = null;
+        }
+    }
 
+    private static boolean hasCycle(ListNode head) {
+        if (head == null || head.next == null) {
+            return false;
+        }
+        ListNode slow = head;
+        ListNode fast = head.next;
+        while (fast != null && fast.next != null) {
+            if (slow == fast) {
+                return true;
+            }
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        return false;
+    }
+
+    public static void main(String[] args) {
+        ListNode head = new ListNode(3);
+        head.next = new ListNode(2);
+        head.next.next = new ListNode(0);
+        head.next.next.next = new ListNode(-4);
+        head.next.next.next.next = head.next; 
+        System.out.println(hasCycle(head));
     }
 
 }
