f27e0479e049c28b7afe80bc18543c895eb76570
saiashish9 <saiashish3760@gmail.com>
2025-09-05 19:09:54 +0530

    feat(leetcode-patterns): updated two pointers solutions

--- twoPointers/q75/SortColors.java
+++ twoPointers/q75/SortColors.java
@@ -1,9 +1,32 @@
 package twoPointers.q75;
 
+import java.util.Arrays;
+
 public class SortColors {
 
     public static void main(String[] args) {
+        int[] nums = { 2, 0, 2, 1, 1, 0 };
 
+        int low = 0, mid = 0, high = nums.length - 1;
+        while (low <= high && mid <= high) {
+            if (nums[mid] == 0) {
+                swap(nums, low, mid);
+                low++;
+                mid++;
+            } else if (nums[mid] == 1) {
+                mid++;
+            } else {
+                swap(nums, mid, high);
+                high--;
+            }
+        }
+        System.out.println(Arrays.toString(nums));
+    }
+
+    private static void swap(int[] nums, int i, int j) {
+        int temp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = temp;
     }
 
 }
