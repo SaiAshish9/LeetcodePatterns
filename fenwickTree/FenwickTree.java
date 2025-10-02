7a6f0e03e9fac28f738b7d0b5af1804114d752f4
saiashish9 <saiashish3760@gmail.com>
2025-10-02 15:59:59 +0530

    feat(leetcode-patterns): solutions updated

--- fenwickTree/FenwickTree.java
+++ fenwickTree/FenwickTree.java
@@ -3,7 +3,7 @@ package fenwickTree;
 public class FenwickTree {
 
     public static void main(String[] args) {
-        System.out.println("Fenwick Tree Implementation")
+        System.out.println("Fenwick Tree Implementation");
     }
     
 }
