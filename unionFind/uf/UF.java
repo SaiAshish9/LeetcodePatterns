1c8699962ab2d7473f537c48bbf238ac956f01d7
saiashish9 <saiashish3760@gmail.com>
2025-10-03 13:41:29 +0530

    feat(leetcode-patterns): solutions updated

---
+++ unionFind/uf/UF.java
@@ -0,0 +1,56 @@
+package unionFind.uf;
+
+public class UF {
+    static class UnionFind {
+        int[] parent;
+
+        UnionFind(int n) {
+            parent = new int[n];
+            for (int i = 0; i < n; i++) {
+                parent[i] = i;
+            }
+        }
+
+        public int find(int u) {
+            if (u != parent[u]) {
+                parent[u] = find(parent[u]); // path compression
+            }
+            return parent[u];
+        }
+
+        public boolean union(int u, int v) {
+            int x = find(u);
+            int y = find(v);
+
+            if (x == y)
+                return false;
+
+            parent[v] = u;
+
+            return true;
+        }
+    }
+
+    public static void main(String... s) {
+        UnionFind uf = new UnionFind(5);
+        System.out.println(uf.find(1));
+        uf.union(1, 2);
+        uf.union(2, 3);
+        System.out.println(uf.find(1));
+        System.out.println(uf.find(2));
+        System.out.println(uf.find(3));
+    }
+
+}
+
+/*
+ * 1
+ * 1
+ * 1
+ * 1
+ */
+
+// Complexity Analysis
+// Time Complexity: O(N) for the find operation in the worst case, where N is
+// the number of elements. The union operation is O(1).
+// Space Complexity: O(N) for storing the parent array.
