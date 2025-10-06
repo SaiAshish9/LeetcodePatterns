26959e681a5efd6e94e3bd0e82755831a1c42f15
saiashish9 <saiashish3760@gmail.com>
2025-10-06 14:52:19 +0530

    feat(codeforces): rating updated

--- kruskalMST/q1489/CriticalAndPsuedoCritical.java
+++ kruskalMST/q1489/CriticalAndPsuedoCritical.java
@@ -54,10 +54,10 @@ class CriticalAndPsuedoCritical {
 
         Arrays.sort(edges, (a, b) -> a[2] - b[2]);
 
-        final int mstWeight = getMSTWeight(n, edges, new int[] {}, -1);
+        int mstWeight = getMSTWeight(n, edges, new int[] {}, -1);
 
         for (int[] edge : edges) {
-            final int index = edge[3];
+            int index = edge[3];
             // Deleting the `edge` increases the MST's weight or makes the MST invalid.
             if (getMSTWeight(n, edges, new int[] {}, index) > mstWeight)
                 criticalEdges.add(index);
@@ -79,19 +79,17 @@ class CriticalAndPsuedoCritical {
         }
 
         for (int[] edge : edges) {
-            final int u = edge[0];
-            final int v = edge[1];
-            final int weight = edge[2];
-            final int index = edge[3];
+            int u = edge[0];
+            int v = edge[1];
+            int weight = edge[2];
+            int index = edge[3];
             if (index == deletedEdgeIndex)
-                continue;
-            if (uf.find(u) == uf.find(v))
                 continue;
             uf.union(u, v);
             mstWeight += weight;
         }
 
-        final int root = uf.find(0);
+        int root = uf.find(0);
         for (int i = 0; i < n; ++i)
             if (uf.find(i) != root)
                 return Integer.MAX_VALUE;
@@ -102,17 +100,14 @@ class CriticalAndPsuedoCritical {
     public static void main(String[] args) {
         CriticalAndPsuedoCritical sol = new CriticalAndPsuedoCritical();
         int n = 14;
-        int[][] edges = {
-                { 0, 1, 13 }, { 0, 2, 6 }, { 2, 3, 13 }, { 3, 4, 4 }, { 0, 5, 11 }, { 4, 6, 14 }, { 4, 7, 8 },
-                { 2, 8, 6 }, { 4, 9, 6 }, { 7, 10, 4 }, { 5, 11, 3 }, { 6, 12, 7 }, { 12, 13, 9 }, { 7, 13, 2 },
-                { 5, 13, 10 }, { 0, 6, 4 }, { 2, 7, 3 }, { 0, 7, 8 }, { 1, 12, 9 }, { 10, 12, 11 }, { 1, 2, 7 },
-                { 1, 3, 10 }, { 3, 10, 6 }, { 6, 10, 4 }, { 4, 8, 5 }, { 1, 13, 4 }, { 11, 13, 8 }, { 2, 12, 10 },
-                { 5, 8, 1 }, { 3, 7, 6 }, { 7, 12, 12 }, { 1, 7, 9 }, { 5, 9, 1 }, { 2, 13, 10 }, { 10, 11, 4 },
-                { 3, 5, 10 }, { 6, 11, 14 }, { 5, 12, 3 }, { 0, 8, 13 }, { 8, 9, 1 }, { 3, 6, 8 }, { 0, 3, 4 },
-                { 2, 9, 6 }, { 0, 11, 4 }, { 2, 5, 14 }, { 4, 11, 2 }, { 7, 11, 11 }, { 1, 11, 6 }, { 2, 10, 12 },
-                { 0, 13, 4 }, { 3, 9, 9 }, { 4, 12, 3 }, { 6, 7, 10 }, { 6, 8, 13 }, { 9, 11, 3 }, { 1, 6, 2 },
-                { 2, 4, 12 }, { 0, 10, 3 }, { 3, 12, 1 }, { 3, 8, 12 }, { 1, 8, 6 }, { 8, 13, 2 }, { 10, 13, 12 },
-                { 9, 13, 11 }, { 2, 11, 14 }, { 5, 10, 9 }, { 5, 6, 10 }, { 2, 6, 9 }, { 8, 11, 3 }
+        int[][] edges = new int[][]{
+            {0, 1, 1}, 
+            {1, 2, 1},
+            {2, 3, 2},
+            {0, 3, 2},
+            {0, 4, 3},
+            {3, 4, 3},
+            {1, 4, 6},
         };
         List<List<Integer>> result = sol.findCriticalAndPseudoCriticalEdges(n, edges);
         System.out.println(result); 
