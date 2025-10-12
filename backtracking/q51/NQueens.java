b75d0b44ae79ef35af05e9fb185e572f6f55673b
saiashish9 <saiashish3760@gmail.com>
2025-10-12 15:00:36 +0530

    feat(leetcode-patterns): solutions updated

--- backtracking/q51/NQueens.java
+++ backtracking/q51/NQueens.java
@@ -6,43 +6,55 @@ public class NQueens {
 
     private static boolean isSafe(char[][] board, int row, int col, int n) {
         for (int i = 0; i < n; i++) {
-            if (board[row][i] == 'Q')
-                return false;
+            if (board[row][i] == 'Q') return false;
         }
 
         for (int i = 0; i < n; i++) {
-            if (board[i][col] == 'Q')
-                return false;
+            if (board[i][col] == 'Q') return false;
         }
 
         // UL
         for (int i = row, j = col; i >= 0 && j >= 0; i--, j--) {
-            if (board[i][j] == 'Q')
-                return false;
+            if (board[i][j] == 'Q') return false;
         }
 
         // UR
         for (int i = row, j = col; i >= 0 && j < n; i--, j++) {
-            if (board[i][j] == 'Q')
-                return false;
+            if (board[i][j] == 'Q') return false;
         }
 
         return true;
     }
 
-    private static void dfs(char[][] board, int row, int n, int[] count) {
+    private static void dfs(char[][] board, int row, int n, List<List<String>> res) {
         if (row == n) {
-            count[0]++;
+            res.add(construct(board, n));
             return;
         }
 
         for (int col = 0; col < n; col++) {
             if (isSafe(board, row, col, n)) {
                 board[row][col] = 'Q';
-                dfs(board, row + 1, n, count);
+                dfs(board, row + 1, n, res);
                 board[row][col] = '.';
             }
         }
+    }
+    
+    private static List<String> construct(char[][] board, int n) {
+        List<String> res = new ArrayList<>();
+        for (int i=0; i<n; i++) {
+            StringBuilder sb = new StringBuilder();
+            for (int j=0; j<n; j++) {
+                if (board[i][j] == 'Q') {
+                    sb.append("Q");
+                } else {
+                    sb.append(".");
+                }
+            }
+            res.add(sb.toString());
+        }
+        return res;
     }
 
     public static void main(String... s) {
@@ -51,17 +63,17 @@ public class NQueens {
         for (int i = 0; i < n; i++) {
             Arrays.fill(board[i], '.');
         }
-
-        int[] count = new int[] { 0 };
-        dfs(board, 0, n, count);
-        System.out.println(count[0]);
+        List<List<String>> res = new ArrayList<>();
+        dfs(board, 0, n, res);
+        System.out.println(res);
+        // [[.Q.., ...Q, Q..., ..Q.], [..Q., Q..., ...Q, .Q..]]
     }
 }
 
 // Complexity Analysis
-// Time Complexity: O(N!), where N is the number of queens. This is because, in the
-// worst case, we have to explore all the configurations of the board. However,
-// the actual time complexity is much less due to the pruning of invalid
-// configurations using the isSafe function.
-// Space Complexity: O(N^2) for the board and O(N) for the recursion stack, leading to a total
-// space complexity of O(N^2).
+// Time Complexity: O(N!), where N is the number of queens. In the worst case, we
+// have to explore all the configurations of the board. However, this is not the
+// tight bound as many configurations will be pruned.
+// Space Complexity: O(N^2), where N is the number of queens. We are using an
+// auxiliary board of size N x N. The recursion stack can go as deep as N, but
+// this is not the tight bound as well.
