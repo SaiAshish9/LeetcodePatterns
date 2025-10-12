069c812e54e4b08b2b9da9bf51a449fa19348dca
saiashish9 <saiashish3760@gmail.com>
2025-10-12 16:05:49 +0530

    feat(leetcode-patterns): solutions updated

---
+++ backtracking/q93/RestoreIPAddresses.java
@@ -0,0 +1,13 @@
+package backtracking.q93;
+
+public class RestoreIPAddresses {
+
+    public static void main(String[] args) {
+        
+    }
+    
+}
+
+// Complexity Analysis
+// Time Complexity: O(1) (since the number of segments is fixed to 4, the maximum number of valid IP addresses is limited).
+// Space Complexity: O(1) (the recursion stack can go up to a maximum depth of 4, which is constant).
