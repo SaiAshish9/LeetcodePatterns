8fd2165139562e8e9373176a4190758974d2464d
saiashish9 <saiashish3760@gmail.com>
2025-10-15 00:35:08 +0530

    feat(leetcode-patterns): solutions updated

---
+++ matrixes/q74/SearchA2DMatrix.java
@@ -0,0 +1,44 @@
+package matrixes.q74;
+
+public class SearchA2DMatrix {
+
+    private static boolean searchMatrix(int[][] matrix, int target) {
+        if (matrix == null || matrix.length == 0 || matrix[0].length == 0) {
+            return false;
+        }
+
+        int m = matrix.length;
+        int n = matrix[0].length;
+
+        int left = 0;
+        int right = m * n - 1;
+
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            int midValue = matrix[mid / n][mid % n];
+
+            if (midValue == target) {
+                return true;
+            } else if (midValue < target) {
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }
+
+        return false;
+    }
+
+    public static void main(String[] args) {
+        int[][] matrix = {
+                {1, 3, 5, 7},
+                {10, 11, 16, 20},
+                {23, 30, 34, 60}
+        };
+        int target = 3;
+        System.out.println(searchMatrix(matrix, target)); // Output: true
+    }
+}
+
+// Time Complexity: O(log(m * n)) where m is the number of rows and n is the number of columns in the matrix.
+// Space Complexity: O(1)
