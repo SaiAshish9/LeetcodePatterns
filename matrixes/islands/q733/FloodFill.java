ddc6fa73be0b352a588c0d4a235a1add3a037212
saiashish9 <saiashish3760@gmail.com>
2025-10-14 15:37:19 +0530

    feat(leetcode-patterns): solutions updated

--- matrixes/islands/q733/FloodFill.java
+++ matrixes/islands/q733/FloodFill.java
@@ -4,7 +4,7 @@ public class FloodFill {
     // Directions: right, down, left, up
     private static final int[][] DIRECTIONS = {{0, 1}, {1, 0}, {0, -1}, {-1, 0}};
     
-    public int[][] floodFill(int[][] image, int sr, int sc, int newColor) {
+    private static int[][] floodFill(int[][] image, int sr, int sc, int newColor) {
         int originalColor = image[sr][sc];
         
         // If the original color is the same as the new color, no need to proceed
@@ -15,7 +15,7 @@ public class FloodFill {
         return image;
     }
     
-    private void dfs(int[][] image, int i, int j, int newColor, int originalColor) {
+    private static void dfs(int[][] image, int i, int j, int newColor, int originalColor) {
         if (i < 0 || i >= image.length || j < 0 || j >= image[0].length || image[i][j] != originalColor) {
             return;
         }
@@ -28,4 +28,31 @@ public class FloodFill {
             dfs(image, newRow, newCol, newColor, originalColor);
         }
     }
+
+    public static void main(String[] args) {
+        int[][] image = {
+            {1, 1, 1},
+            {1, 1, 0},
+            {1, 0, 1}
+        };
+        int sr = 1, sc = 1, newColor = 2;
+        
+        int[][] result = floodFill(image, sr, sc, newColor);
+        
+        for (int[] row : result) {
+            for (int pixel : row) {
+                System.out.print(pixel + " ");
+            }
+            System.out.println();
+        }
+    }
 }
+
+// Output:
+// 2 2 2
+// 2 2 0
+// 2 0 1
+
+// Complexity Analysis
+// Time Complexity: O(N), where N is the number of pixels in the image. In the worst case, we might need to visit every pixel.
+// Space Complexity: O(N) in the worst case due to the recursion stack. In the best case (when the area to be filled is small), the space complexity can be O(1).
