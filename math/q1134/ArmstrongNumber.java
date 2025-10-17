903a937ca2ef6c30e029fd39126c126e5a13f92a
saiashish9 <saiashish3760@gmail.com>
2025-10-17 10:10:13 +0530

    feat(leetcode-patterns): solutions updated

--- math/q1134/ArmstrongNumber.java
+++ math/q1134/ArmstrongNumber.java
@@ -1,9 +1,27 @@
 package math.q1134;
 
 public class ArmstrongNumber {
-    
-    public static void main(String[] args) {
 
+    private static boolean isArmstrong(int n) {
+        String s = Integer.toString(n);
+        int k = s.length();
+        int sum = 0, temp = n;
+        while (temp > 0) {
+            int digit = temp % 10;
+            sum += Math.pow(digit, k);
+            temp /= 10;
+        }
+        return sum == n;
+    }
+
+    public static void main(String[] args) {
+      System.out.println(isArmstrong(0)); // true
+      System.out.println(isArmstrong(153)); // true
+      System.out.println(isArmstrong(9474)); // true
     }
 
 }
+
+// Complexity Analysis
+// Time Complexity: O(d), where d is the number of digits in n.
+// Space Complexity: O(1)
