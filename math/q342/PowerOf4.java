40ca3a6f7bb3adc351b2cec470122fc1ab3cf8e9
saiashish9 <saiashish3760@gmail.com>
2025-10-17 10:01:33 +0530

    feat(leetcode-patterns): solutions updated

--- math/q342/PowerOf4.java
+++ math/q342/PowerOf4.java
@@ -1,9 +1,14 @@
 package math.q342;
 
 public class PowerOf4 {
-    
-    public static void main(String[] args) {
 
+    private static boolean isPowerOfFour(int n) {
+        return n > 0 && (n & (n - 1)) == 0 && (n - 1) % 3 == 0;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(isPowerOfFour(016)); // true
+        System.out.println(isPowerOfFour(32)); // false
     }
 
 }
