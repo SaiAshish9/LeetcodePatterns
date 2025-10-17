3c32980ca48257680fc906087c96058ecc642f6b
saiashish9 <saiashish3760@gmail.com>
2025-10-17 10:00:01 +0530

    feat(leetcode-patterns): solutions updated

--- math/q231/PowerOf2.java
+++ math/q231/PowerOf2.java
@@ -1,9 +1,19 @@
 package math.q231;
 
 public class PowerOf2 {
-    
-    public static void main(String[] args) {
 
+    private static boolean isPowerOfTwo(int n) {
+        return n > 0 && (n & (n - 1)) == 0;
+    }
+
+    public static void main(String[] args) {
+      System.out.println(isPowerOfTwo(016)); // true
+      System.out.println(isPowerOfTwo(218)); // false
     }
 
 }
+
+
+// Complexity Analysis  
+// Time Complexity: O(1)  
+// Space Complexity: O(1)
