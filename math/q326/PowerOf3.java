e9a51b19e08f09b16bb3dfff595f93053d632a33
saiashish9 <saiashish3760@gmail.com>
2025-10-17 10:00:56 +0530

    feat(leetcode-patterns): solutions updated

--- math/q326/PowerOf3.java
+++ math/q326/PowerOf3.java
@@ -2,8 +2,21 @@ package math.q326;
 
 public class PowerOf3 {
 
-    public static void main(String[] args) {
+    private static boolean isPowerOfThree(int n) {
+        if (n <= 0) return false;
+        while (n % 3 == 0) {
+            n /= 3;
+        }
+        return n == 1;
+    }
 
+    public static void main(String[] args) {
+       System.out.println(isPowerOfThree(027)); // true
+       System.out.println(isPowerOfThree(45)); // false
     }
     
 }
+
+// Complexity Analysis  
+// Time Complexity: O(log3(n))  
+// Space Complexity: O(1)
