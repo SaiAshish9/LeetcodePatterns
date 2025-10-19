4338700ad9981097a8f3523026f3a8d6036f7838
saiashish9 <saiashish3760@gmail.com>
2025-10-20 02:25:42 +0530

    feat(leetcode-patterns): solutions updated

---
+++ stringMatching/kmp/LongestPrefixSuffix.java
@@ -0,0 +1,31 @@
+package stringMatching.kmp;
+
+public class LongestPrefixSuffix {
+
+    private static void computeLPSArray(String str, int n, int[] lps) {
+        int length = 0;
+        int i = 1;
+        lps[0] = 0;
+
+        while (i < n) {
+            if (str.charAt(i) == str.charAt(length)) {
+                lps[i++] = ++length;
+            } else {
+                if (length != 0) {
+                    length = lps[length - 1];
+                } else {
+                    lps[i++] = 0;
+                }
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        String str = "ababcababc";
+        int n = str.length();
+        int[] lps = new int[n];
+        computeLPSArray(str, n, lps);
+        System.out.println("Length of Longest Prefix which is also Suffix: " + lps[n - 1]);
+    }
+
+}
