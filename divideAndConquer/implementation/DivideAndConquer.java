17f2e3625e39a4c13307aa341b41b1c6dd0f6e83
saiashish9 <saiashish3760@gmail.com>
2025-10-20 01:54:13 +0530

    feat(leetcode-patterns): solutions updated

--- divideAndConquer/implementation/DivideAndConquer.java
+++ divideAndConquer/implementation/DivideAndConquer.java
@@ -1,9 +1,36 @@
 package divideAndConquer.implementation;
 
+import java.util.Arrays;
+
 public class DivideAndConquer {
+    public static void mergeSort(int[] arr, int l, int r) {
+        if (l < r) {
+            int mid = l + (r - l) / 2;
+            mergeSort(arr, l, mid);
+            mergeSort(arr, mid + 1, r);
+            merge(arr, l, mid, r);
+        }
+    }
+
+    private static void merge(int[] arr, int l, int m, int r) {
+        int[] left = Arrays.copyOfRange(arr, l, m + 1);
+        int[] right = Arrays.copyOfRange(arr, m + 1, r + 1);
+
+        int i = 0, j = 0, k = l;
+
+        while (i < left.length && j < right.length) {
+            arr[k++] = (left[i] <= right[j]) ? left[i++] : right[j++];
+        }
+
+        while (i < left.length)
+            arr[k++] = left[i++];
+        while (j < right.length)
+            arr[k++] = right[j++];
+    }
 
     public static void main(String[] args) {
-        
+        int[] arr = { 5, 2, 4, 6, 1, 3 };
+        mergeSort(arr, 0, arr.length - 1);
+        System.out.println(Arrays.toString(arr));
     }
-    
 }
