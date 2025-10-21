e258c4983f19738efcdf5c99fdf993a52f16155c
saiashish9 <saiashish3760@gmail.com>
2025-10-21 15:00:33 +0530

    feat(leetcode-patterns): solutions updated

---
+++ sorting/comparison/quick/Quick.java
@@ -0,0 +1,43 @@
+package sorting.comparison.quick;
+
+public class Quick {
+
+    private static void quickSort(int[] arr, int low, int high) {
+        if (low < high) {
+            int pi = partition(arr, low, high);
+            quickSort(arr, low, pi - 1);
+            quickSort(arr, pi + 1, high);
+        }
+    }
+
+    private static int partition(int[] arr, int low, int high) {
+        int pivot = arr[high];
+        int i = (low - 1);
+        for (int j = low; j < high; j++) {
+            if (arr[j] < pivot) {
+                i++;
+                int temp = arr[i];
+                arr[i] = arr[j];
+                arr[j] = temp;
+            }
+        }
+        int temp = arr[i + 1];
+        arr[i + 1] = arr[high];
+        arr[high] = temp;
+        return i + 1;
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {3,6,8,10,1,2,1};
+        quickSort(arr, 0, arr.length - 1);
+        for (int num : arr) {
+            System.out.print(num + " ");
+        } // Output: 1 1 2 3 6 8 10
+    }
+    
+}
+
+
+// Complexity Analysis:
+// Time Complexity: O(n log n) on average, O(n^2) in the worst case.
+// Space Complexity: O(log n) due to recursive stack space. 
